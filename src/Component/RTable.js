71be274c6fc028f7a81bf95c799bfb2b88863c76
duyet1905 <72645374+duyet1905@users.noreply.github.com>
2021-05-03 08:24:38 +0700

    Update RTable.js

--- src/Component/RTable.js
+++ src/Component/RTable.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 import "rsuite/lib/styles/index.less";
 import "rsuite/dist/styles/rsuite-default.css";
-import { Button, Table } from "rsuite";
+import { Button, Table, Checkbox } from "rsuite";
+import example from "../Component/productarr";
 
 const { Column, HeaderCell, Cell } = Table;
+
 const styles = {
+  
   btn: {
-    backgroundColor: "#169de0",
-    color: "#fff",
+    backgroundColor: "#000000",
+    color: "#A9A9A9",
   },
   table: {
     width: "710px",
-     border: '3px solid #73AD21'
+     border: '1px solid #000000'
+
   },
 };
+const CheckCell = ({ rowData, onChange, checkedKeys, dataKey, ...props }) => (
+  <Cell {...props} style={{ padding: 0 }}>
+    <div style={{ lineHeight: '46px' }}>
+      <Checkbox
+        value={rowData[dataKey]}
+        inline
+        onChange={onChange}
+        checked={checkedKeys.some(item => item === rowData[dataKey])}
+      />
+    </div>
+  </Cell>
+);
 
+const dataT = example.filter((v, i) => i < 20);
 class RTable extends Component {
   getData() {
     let { ArrayProduct = [], ArrayTemporary = [], keyWord } = this.props;
@@ -22,30 +39,111 @@ class RTable extends Component {
     return listData;
   }
 
+   constructor(props) {
+    super(props);
+    this.state = {
+      checkedKeys: [],
+      dataT
+    };
+    this.handleCheckAll = this.handleCheckAll.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+  handleCheckAll(value, checked) {
+    const checkedKeys = checked ? dataT.map(item => item.id) : [];
+    this.setState({
+      checkedKeys
+    });
+  }
+   handleCheck(value, checked) {
+    const { checkedKeys } = this.state;
+    const nextCheckedKeys = checked
+      ? [...checkedKeys, value]
+      : checkedKeys.filter(item => item !== value);
+
+    this.setState({
+      checkedKeys: nextCheckedKeys
+    });
+  }
+
   render() {
-    let { showFormEdit, deleteData } = this.props;
+ const { dataT, checkedKeys } = this.state;
+
+    let checked = false;
+    let indeterminate = false;
+
+    if (checkedKeys.length === dataT.length) {
+      checked = true;
+    } else if (checkedKeys.length === 0) {
+      checked = false;
+    } else if (checkedKeys.length > 0 && checkedKeys.length < dataT.length) {
+      indeterminate = true;
+    }
+
+
+    let { showFormEdit, deleteData,  deleteDataTable } = this.props;
     const data = this.getData();
     return (
       <div>
-        <Table style={styles.table} data={data} height={400}>
+      <Button
+                      style={styles.btn}
+                      onClick={() => deleteDataTable(checkedKeys)}
+                    >
+                    Xóa mục đã chọn
+        </Button>
+          <Table id = "dataTable" style={styles.table} data={data} height={400}>
+
+
+          <Column width={70} align="center">
+            <HeaderCell>
+              <div>
+                <Checkbox
+                  inline
+                  checked={checked}
+                  indeterminate={indeterminate}
+                  onChange={this.handleCheckAll}
+                />
+                </div>
+              
+            </HeaderCell>   
+         
+                     <CheckCell
+              dataKey="id"
+              checkedKeys={checkedKeys}
+              onChange={this.handleCheck}
+            />      
+          </Column>
+
+
+
+
+
           <Column width={70} align="center">
             <HeaderCell>Id</HeaderCell>
             <Cell dataKey="id" />
           </Column>
+
+
           <Column width={220}>
             <HeaderCell>Tên Sản Phẩm</HeaderCell>
             <Cell dataKey="name" />
           </Column>
+
+
           <Column width={100}>
             <HeaderCell>Ghi chú</HeaderCell>
             <Cell dataKey="note" />
           </Column>
+
+
           <Column width={100} flexGrow={1}>
             <HeaderCell>Giá</HeaderCell>
             <Cell dataKey="price" />
           </Column>
+
+
+
           <Column width={120} fixed="right">
-            <HeaderCell>Action</HeaderCell>
+            <HeaderCell>Action</HeaderCell>   
             <Cell>
               {(rowIndex) => {
                 return (
@@ -69,8 +167,10 @@ class RTable extends Component {
               }}
             </Cell>
           </Column>
-        </Table>
-      </div>
+    </Table>
+
+          </div>
+    
     );
   }
 }
