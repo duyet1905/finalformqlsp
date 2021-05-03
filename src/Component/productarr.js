252088e109184bfa04e56f407e2a127612bd1d66
duyet1905 <72645374+duyet1905@users.noreply.github.com>
2021-05-03 08:23:56 +0700

    Add files via upload

---
+++ src/Component/productarr.js
@@ -0,0 +1,28 @@
+const example = [
+  {
+    id : "123",
+    name: "abc",
+    note: "def",
+    price: "5000",
+  },
+  {
+    id : "234",
+    name: "abd",
+    note: "dec",
+    price: "5000",
+  },
+  {
+    id : "126",
+    name: "aaa",
+    note: "def",
+    price: "5000",
+  },
+  {
+    id : "16",
+    name: "cca",
+    note: "def",
+    price: "5000",
+  },
+];
+export default example;
+  
